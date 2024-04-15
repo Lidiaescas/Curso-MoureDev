be3e8a5ec7d9ae78a9b27eb2925c305d127ff9df
Lidiaescas <lidia.castellanos15@gmail.com>
2024-04-15 23:51:40 +0200

    ejercicio 1 y 2 de blucles

--- Bucles/004_ej1.py
+++ Bucles/004_ej1.py
@@ -1,12 +1,20 @@
 # 1 Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
 
-palabra = input("dame una palabreta: ")
+#palabra = input("dame una palabreta: ")
 i = 8
 
 while i <=10:
-    print(palabra)
+   # print(palabra)
     i = i + 1
 
 
 
+""" Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años 
+que ha cumplido (desde 1 hasta su edad)."""
 
+edad = int(input("Cuantos años tienes?: "))
+i = 1
+
+while i <= edad:
+    print(i)
+    i = i + 1
