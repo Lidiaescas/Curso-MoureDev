2be2b98eb220692f113592575340fbcef66cb071
Lidiaescas <lidia.castellanos15@gmail.com>
2024-04-18 17:50:28 +0200

    funciones

--- 10_funtions.py
+++ 10_funtions.py
@@ -1,1 +1,15 @@
 ### Funciones ###
+
+def my_function (): #Como definir una función
+    print("Esto es una funcion")
+
+
+my_function()
+
+
+def sum_two_values(first_number, second_number):
+    print(first_number + second_number)
+
+
+sum_two_values()
+
