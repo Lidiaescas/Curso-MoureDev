901f85988fdcd3cf2fe45faf926886a955d7de54
Lidiaescas <lidia.castellanos15@gmail.com>
2024-04-23 11:06:11 +0200

    funciones

--- 10_funtions.py
+++ 10_funtions.py
@@ -1,15 +1,28 @@
 ### Funciones ###
 
-def my_function (): #Como definir una función
+#Funciones normales
+
+def my_function (): #función def: Como definir una función
     print("Esto es una funcion")
 
 
 my_function()
 
 
+#Funciones para pasar datos
+
 def sum_two_values(first_number, second_number):
     print(first_number + second_number)
 
 
-sum_two_values()
+sum_two_values(5, 7)
+#Si ponemos valores que se puedan concatenar la operación se hará sin problema
+
+
+
+#La función al igual que puede recibir parametros los puede retornar
+def sum_two_values_with_return(first_number, second_number):
+    return(first_number + second_number)
+
+my_result = sum_two_values_with_return(10, 5)
 
