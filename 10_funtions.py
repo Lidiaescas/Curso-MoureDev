3ab73acc376f319ea525722307742b53db7e914c
Lidiaescas <lidia.castellanos15@gmail.com>
2024-04-23 17:44:22 +0200

    j

--- 10_funtions.py
+++ 10_funtions.py
@@ -25,4 +25,5 @@ def sum_two_values_with_return(first_number, second_number):
     return(first_number + second_number)
 
 my_result = sum_two_values_with_return(10, 5)
-
+my_result = sum_two_values (1.4, 5.2)
+print(my_result) #Esta función no retorna nada
