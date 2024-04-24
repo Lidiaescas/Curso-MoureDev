f39e3de1e367be7715df1fb3f1b4a2c732e7d35d
Lidiaescas <lidia.castellanos15@gmail.com>
2024-04-24 22:53:32 +0200

    continuo con las clases

--- 11_classes.py
+++ 11_classes.py
@@ -1,5 +1,34 @@
 # Classes #
 
 #Cómo definir una clase:
-class Person:
+#Los nombres de las clases se escriben en camel case (MyPerson)
+class MyEmtyPerson:
     pass
+
+print(MyEmtyPerson)
+print(MyEmtyPerson())
+
+#Constructor de clase (siempre necesario el self):
+class Person:
+    def __init__(self, name, surname):
+        self.name = name #Self tiene una propiedad name igualada a name
+        self.surname = surname
+
+my_person = Person("Brais", "Moure")
+print(f"{my_person.name} {my_person.surname}")
+
+
+class Person:
+    def __init__(self, name, surname, alias = "sin alias"):
+        self.full_name = f"{name} {surname} {alias}"
+
+    def walk(self):
+        print(f" {self.full_name} Está caminando")
+
+my_person = Person("Brais", "Moure")
+print(my_person.full_name)
+
+my_person.walk()
+
+
+my_other_person = Person()
