82aa04a09962aa85b1fe0d8d50abea3da15faf99
Lidiaescas <lidia.castellanos15@gmail.com>
2024-04-25 19:02:09 +0200

    ej 1 y 2 funciones

---
+++ Bucles/Funciones/009_ej 1 y 2.py
@@ -0,0 +1,19 @@
+#Escribir una función que muestre por pantalla el saludo ¡Hola amiga! cada vez que se la invoque.
+
+def my_function ():
+    print("Hola amiga!")
+
+my_function()
+
+
+"""Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo 
+¡hola <nombre>!."""
+
+def my_name (name): 
+    print("Hola " + name + "!")
+    
+
+my_name("LIDIA")
+
+
+
