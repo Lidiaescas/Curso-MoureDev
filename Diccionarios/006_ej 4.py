2ff66193f6be71c11432740e7bbb9494c1db22d9
Lidiaescas <lidia.castellanos15@gmail.com>
2024-05-07 18:11:53 +0200

    ejercicios

---
+++ Diccionarios/006_ej 4.py
@@ -0,0 +1,15 @@
+
+"""Escribir un programa que pregunte una fecha en formato dd/mm/aaaa y muestre por pantalla la misma 
+fecha en formato dd de <mes> de aaaa donde <mes> es el nombre del mes."""
+
+meses = {"Enero": 1, "Febrero": 2, "Marzo": 3, "Abril": 4, "Mayo": 5, "Junio": 6, "Julio": 7, "Agosto": 8, "Septiembre": 9, "Octubre": 10, "Noviembre": 11, "Diciembre": 12}
+pregunta_fecha = input("Inserta fecha: ")
+
+
+for mes, valor in meses.items():
+    if pregunta_fecha:
+        separacion = pregunta_fecha.split("/")
+        print(separacion[0],"de", separacion[1], "de", separacion[2])
+        break
+else:
+    print("error")
